c730b9c350a3d23b30fb9473465340083ca0895e
Paoxie <Paoxie@users.noreply.github.com>
2014-07-05 02:37:46 +0200

    Update mygopacket.go

--- mygopacket.go
+++ mygopacket.go
@@ -1,18 +1,18 @@
-    This software is for intended for WPA-2 cracking, with or without ARP Poisoning. - Not yet finished
-    Copyright (C) 2014  Vittus Peter Ove Maqe Mikiassen
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//    This software is for intended for WPA-2 cracking, with or without ARP Poisoning. - Not yet finished
+//    Copyright (C) 2014  Vittus Peter Ove Maqe Mikiassen
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package main
 import (
